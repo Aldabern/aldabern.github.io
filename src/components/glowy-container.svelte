286525f71e39c2d2cb9a3545271e5a49787d86a3
Ashfid <ashfidable.islam@gmail.com>
2024-08-27 01:26:32 +0300

    refactor: glowy container cleanups

--- src/components/glowy-container.svelte
+++ src/components/glowy-container.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
     import type { Snippet } from "svelte";
 
-  let { hover = true, children }: {hover: boolean, children: Snippet} = $props();
+  let { children }: {children: Snippet} = $props();
 </script>
 
-<div class="rounded-md"  data-effect-on-hover={hover}>{@render children()}</div>
+<div class="rounded-md hover:scale-105 transition-transform duration-200 " >{@render children()}</div>
 
 <style>
   @property --gradient-angle {
@@ -23,15 +23,18 @@
     content: "";
     position: absolute;
     inset: -2px;
-    bottom: -4px;
-    opacity: 0;
+    bottom: -6px;
+    opacity: 1;
     z-index: -1;
     border-radius: inherit;
-    transition: opacity 250ms ease-in-out;
+    filter: blur(0rem);
+    transition: filter 0.75s;
+    background-color: #242323;
   }
 
-  div[data-effect-on-hover="false"]::after,
-  div[data-effect-on-hover="false"]::before {
+
+  div:hover::after,
+  div:hover::before {
     background: conic-gradient(
       from var(--gradient-angle),
       var(--clr-1),
@@ -42,24 +45,11 @@
     );
     opacity: 1;
     animation: rotation 5s linear infinite;
+
   }
 
-  div[data-effect-on-hover="true"]:hover::after,
-  div[data-effect-on-hover="true"]:hover::before {
-    background: conic-gradient(
-      from var(--gradient-angle),
-      var(--clr-1),
-      var(--clr-2),
-      var(--clr-3),
-      var(--clr-2),
-      var(--clr-1)
-    );
-    opacity: 1;
-    animation: rotation 5s linear infinite;
-  }
-
-  div::after {
-    filter: blur(0.25rem);
+  div:hover::after {
+    filter: blur(1.75rem);
   }
 
   @keyframes rotation {
